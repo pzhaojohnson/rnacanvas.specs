9692ec29d262ae5e27c1dc35105d1bea5ea9170c
Philip Johnson <p.zhao.johnson@gmail.com>
2025-11-17 23:15:17 -0500

    Initialize repository

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.module\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                namedExport: true,
+                localIdentHashSalt: '760447737869576', // make unique for every package
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    globalObject: 'this',
+    library: {
+      name: 'specs',
+      type: 'umd',
+    },
+    clean: true,
+  },
+};
